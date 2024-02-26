43a46e9e6f072bd005743fc8e603dfee41aaa513
Harmag3ddon <71220141+Harmag3ddon@users.noreply.github.com>
2024-02-26 21:40:44 +0200

    Making loading message appear

--- WpfBlazor/MainWindow.xaml.cs
+++ WpfBlazor/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 
@@ -27,5 +14,6 @@ public partial class MainWindow : Window
         serviceCollection.AddWpfBlazorWebView();
         serviceCollection.AddMudServices();
         Resources.Add("services", serviceCollection.BuildServiceProvider());
+        
     }
 }
